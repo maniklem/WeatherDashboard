430d4be4a873c74f3576e5442a90ea941b8f7e45
divyani1997 <divyanidevadiya@gmail.com>
2020-10-17 17:09:49 +0530

    Initial commit

---
+++ src/components/Content.vue
@@ -0,0 +1,26 @@
+<template>
+  <div style="position: relative;">
+    <temp-var-chart :tempVar="tempVar"></temp-var-chart>
+    <today-highlights :highlights="highlights"></today-highlights>
+  </div>
+</template>
+
+<script>
+import TempVarChart from './TempVarChart.vue'
+import Highlights from './Highlights.vue'
+
+export default {
+    props:['highlights', 'tempVar'],
+    components:{
+        'temp-var-chart':TempVarChart,
+        'today-highlights':Highlights
+    },
+}
+</script>
+
+<style>
+#pagecontent {
+  border: 1px solid black;
+  padding: 2px;
+}
+</style>
